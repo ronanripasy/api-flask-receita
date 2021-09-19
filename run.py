5a2dc0c868b8f28172ce5707c0e4c5faa06dd151
Ronan Ripasy <ronan.ripasy@gmail.com>
2021-09-19 12:48:47 -0400

    adicionando endoint receitas, com dados staticos

--- run.py
+++ run.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template, jsonify
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder="templates")
+
+bootstrap = Bootstrap(app)
+
+receitas = {
+    "receitas": [
+        {
+            "nome": "Arroz com ovo",
+            "tempo_de_preparo": 15,
+            "serve_quantos": 10
+        },
+        {
+            "nome": "Feijão ovo",
+            "tempo_de_preparo": 45,
+            "serve_quantos": 8
+        },
+        {
+            "nome": "Pão com ovo",
+            "tempo_de_preparo": 30,
+            "serve_quantos": 5
+        },
+        {
+            "nome": "Omelete",
+            "tempo_de_preparo": 15,
+            "serve_quantos": 2
+        }
+    ]
+}
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/receitas')
+def lista_receitas():
+    return jsonify(receitas)
 
 
 if __name__ == '__main__':
