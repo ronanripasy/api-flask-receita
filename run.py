4c4fc094255cded65a897106479f119ab2923d6a
Ronan Ripasy <ronan.ripasy@gmail.com>
2021-09-19 14:28:03 -0400

    inserindo nova receita

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime, timedelta
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder="templates")
@@ -10,8 +10,7 @@ bootstrap = Bootstrap(app)
 def return_data(dias):
     return datetime.today() - timedelta(dias)
 
-receitas = {
-    "receitas": [
+receitas = [
         {
             "nome": "Arroz com ovo",
             "tempo_de_preparo": 15,
@@ -37,27 +36,31 @@ receitas = {
             "data": return_data(1).strftime("%d/%m/%Y")
         }
     ]
-}
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/receita')
-def lista_receitas():
-    return jsonify(receitas)
+@app.route('/receita', methods=['GET', 'POST'])
+def receita():
+    data = {}
+    if request.method == 'POST':
+        receita = request.json
+        receitas.append(receita)
+        data = receitas
+    elif request.method == 'GET':
+        data = receitas
+    return jsonify(data)
 
 @app.route('/receita/<int:id>')
 def item_receita(id):
     try:
-        lista_item = receitas['receitas']
-        receita = lista_item[id - 1]
+        receita = receitas[id - 1]
         return jsonify(receita)
     except IndexError as error:
         return 'Receita não encontrada'
     except Exception as e:
         return '404'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
